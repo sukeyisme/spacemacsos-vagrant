5ce4d4b7cc7e995229961fa9215447b4c4ee2dac
hodge <hodge.shen@gmail.com>
2018-01-31 19:51:51 +0800

    update package.sh

--- package.sh
+++ package.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
-vagrant destory default -f
 vagrant up
-vagrant provision --provision-with package-vagrant package-root
 vagrant package --base spacemacsOS
 echo "The vagrant environment is packaged , now you can upload the package."
